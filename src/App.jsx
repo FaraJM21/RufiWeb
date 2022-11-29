addad588703c75f6050d03e2e4c961d55fed65fc
FaraJM21 <farruxrihsiyev@gmail.com>
2022-11-29 17:47:43 +0500

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AboutApartment, Apartment, Home, Layout, Profile } from "./pages";
+import './assets/App.scss'
 function App() {
   return (
-    <div>farrux</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/apartment" element={<Apartment />} />
+          <Route path="/aboutaprtment/:id" element={<AboutApartment />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/aboutapartment/AboutApartment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AboutApartment() {
+  return (
+    <div>AboutApartment</div>
+  )
+}
+
+export default AboutApartment
