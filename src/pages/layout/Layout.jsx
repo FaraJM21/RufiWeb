06b75a8e9431f98214b65e2b50741c506610248e
FaraJM21 <farruxrihsiyev@gmail.com>
2022-11-30 10:40:29 +0500

    header

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Header } from "../../components";
+
 
 function Layout() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
