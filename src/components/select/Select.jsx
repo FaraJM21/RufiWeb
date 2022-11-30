7e73f5b4344ec2b441983b21b976026c888dcc51
FaraJM21 <farruxrihsiyev@gmail.com>
2022-11-30 12:33:20 +0500

    select fixed

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -5,21 +5,23 @@ const handleChange = (value) => {
   console.log(`selected ${value}`);
 };
 const Selects = () => (
-  <Select
-  id="select"
-    defaultValue="Ru"
-    bordered={false}
-    onChange={handleChange}
-    options={[
-      {
-        value: "uz",
-        label: "Uz",
-      },
-      {
-        value: "ru",
-        label: "Ru",
-      },
-    ]}
-  />
+  <div className="myselect">
+    <Select
+      id="select"
+      defaultValue="Ru"
+      bordered={false}
+      onChange={handleChange}
+      options={[
+        {
+          value: "uz",
+          label: "Uz",
+        },
+        {
+          value: "ru",
+          label: "Ru",
+        },
+      ]}
+    />
+  </div>
 );
 export default Selects;
