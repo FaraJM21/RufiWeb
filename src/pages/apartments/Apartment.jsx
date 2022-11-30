58e936aa75681fdc50053231016b6b5360890f76
Dilshodbek <100418811+Dilshodbek98@users.noreply.github.com>
2022-11-30 10:43:20 +0500

    zx

---
+++ src/pages/apartments/Apartment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Apartment() {
+  return (
+    <div className="main">
+      <div className="container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Apartment;
