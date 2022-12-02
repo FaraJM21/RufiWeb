871d2ce97c3e750bf30edafda4c6f6f235da244f
Dilshodbek <100418811+Dilshodbek98@users.noreply.github.com>
2022-12-02 11:10:23 +0500

    search component finished

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import search from "../../assets/img/search.svg";
 import arrow from "../../assets/img/arrow.svg";
+import { Select } from "antd";
 const Search = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
   return (
     <section className="search">
       <div className="wrapper">
@@ -11,26 +16,100 @@ const Search = () => {
           <input type="text" placeholder="Введите город, адресс, район" />
           <p className="input-text">Ташкент</p>
         </div>
-        <select name="select" id="">
-          <option value="buy">Купить</option>
-          <option value="rent">Снять</option>
-        </select>
-        <select name="select" id="" className="big-select">
-          <option value="oldHouse">Квартира во вторичке</option>
-          <option value="newHouse">Квартира в новостойке</option>
-        </select>
-        <select name="select" id="" defaultValue={"Комнат"}>
-          <option value="0">Комнат</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-        </select>
 
-        <div className="price-field">
+        {/* <select name="select" id="">
+          <option value="buy"></option>
+          <option value="rent"></option>
+        </select> */}
+        <Select
+          defaultValue="buy"
+          popupClassName="submenu"
+          dropdownStyle={{ backgroundColor: "white" }}
+          onChange={() => {}}
+          options={[
+            {
+              value: "buy",
+              label: "Купить",
+            },
+            {
+              value: "rent",
+              label: "Снять",
+            },
+          ]}
+        />
+        <Select
+          defaultValue="oldHouse"
+          popupClassName="submenu"
+          dropdownStyle={{ backgroundColor: "white" }}
+          onChange={() => {}}
+          options={[
+            {
+              value: "oldHouse",
+              label: "Квартира во вторичке",
+            },
+            {
+              value: "newHouse",
+              label: "Квартира в новостойке",
+            },
+          ]}
+        />
+        <Select
+          defaultValue="Комнат"
+          popupClassName="submenu"
+          dropdownStyle={{ backgroundColor: "white" }}
+          onChange={() => {}}
+          options={[
+            {
+              value: "1",
+              label: "1",
+            },
+            {
+              value: "2",
+              label: "2",
+            },
+            {
+              value: "3",
+              label: "3",
+            },
+            {
+              value: "4",
+              label: "4",
+            },
+          ]}
+        />
+
+        <div className="price-field" onClick={() => setDropdown(!dropdown)}>
           <span>Цена</span>
           <img src={arrow} alt="" />
+          <div
+            className={dropdown ? "dropdown-menu active" : "dropdown-menu"}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="min">
+              <input
+                type="number"
+                placeholder="от"
+                onKeyDown={(e) => {
+                  if (e.code === "Enter") {
+                    setDropdown(!dropdown);
+                  }
+                }}
+              />
+            </div>
+            <div className="max">
+              <input
+                type="number"
+                placeholder="до"
+                onKeyDown={(e) => {
+                  if (e.code === "Enter") {
+                    setDropdown(!dropdown);
+                  }
+                }}
+              />
+            </div>
+          </div>
         </div>
+
         <div className="search-icon">
           <img src={search} alt="" />
         </div>
