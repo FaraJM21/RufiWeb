a3a5653be65c388864c4ac3d5f5462c0a823bf8a
Dilshodbek <100418811+Dilshodbek98@users.noreply.github.com>
2022-12-05 12:47:32 +0500

    mobile ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {   AbaoutApartment, Apartments, Home, Layout, Profile } from "./pages";
-import './assets/App.scss'
+import { AboutApartment, Apartments, Home, Layout, Profile } from "./pages";
+import "./assets/App.scss";
 import "swiper/css";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/apartments" element={<Apartments />} />
-          <Route path="/aboutapartment/:id" element={<AbaoutApartment />} />
+          <Route path="/aboutapartment/:id" element={<AboutApartment />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -12,22 +12,8 @@ const Search = () => {
     console.log(`selected ${value}`);
   };
 
-  //for ant dropdown
-  const items = [
-    {
-      label: (
-        <div>
-          <div className="sub-menu">
-            <h1>qweqw</h1>
-            <h1>qweqw</h1>
-            <h1>qweqw</h1>
-            <h1>qweqw</h1>
-          </div>
-        </div>
-      ),
-      key: "item-1",
-    }, // remember to pass the key prop
-  ];
+  //for mobile search - filter
+  const [mobileDropdown, setMobileDropdown] = useState(false);
 
   return (
     <section className="search">
@@ -41,7 +27,6 @@ const Search = () => {
 
         <Select
           defaultValue="buy"
-          popupClassName="submenu"
           dropdownStyle={{ backgroundColor: "white" }}
           onChange={() => {}}
           options={[
@@ -57,7 +42,6 @@ const Search = () => {
         />
         <Select
           defaultValue="oldHouse"
-          popupClassName="submenu"
           dropdownStyle={{ backgroundColor: "white" }}
           onChange={() => {}}
           options={[
@@ -73,7 +57,6 @@ const Search = () => {
         />
         <Select
           defaultValue="Комнат"
-          popupClassName="submenu"
           dropdownStyle={{ backgroundColor: "white" }}
           onChange={() => {}}
           options={[
@@ -136,9 +119,104 @@ const Search = () => {
       {/* mobile version */}
       <div className="mobile">
         <input type="text" placeholder="Адресс, метро, район " />
-        <Dropdown menu={{ items }} trigger="click">
-          <a>Фильтры</a>
-        </Dropdown>
+        <p onClick={() => setMobileDropdown(!mobileDropdown)}>Фильтры</p>
+
+        <div className={mobileDropdown ? "submenu active" : "submenu"}>
+          <input
+            type="text"
+            placeholder="Введите город, адресс, район "
+            className="region-input"
+          />
+          <Select
+            defaultValue="buy"
+            dropdownStyle={{ backgroundColor: "white" }}
+            onChange={() => {}}
+            options={[
+              {
+                value: "buy",
+                label: "Купить",
+              },
+              {
+                value: "rent",
+                label: "Снять",
+              },
+            ]}
+          />
+          <Select
+            defaultValue="oldHouse"
+            dropdownStyle={{ backgroundColor: "white" }}
+            onChange={() => {}}
+            options={[
+              {
+                value: "oldHouse",
+                label: "Квартира во вторичке",
+              },
+              {
+                value: "newHouse",
+                label: "Квартира в новостойке",
+              },
+            ]}
+          />
+          <Select
+            defaultValue="Комнат"
+            dropdownStyle={{ backgroundColor: "white" }}
+            onChange={() => {}}
+            options={[
+              {
+                value: "1",
+                label: "1",
+              },
+              {
+                value: "2",
+                label: "2",
+              },
+              {
+                value: "3",
+                label: "3",
+              },
+              {
+                value: "4",
+                label: "4",
+              },
+            ]}
+          />
+          <div className="price-field" onClick={() => setDropdown(!dropdown)}>
+            <span>Цена</span>
+            <img src={arrow} alt="" />
+            <div
+              className={dropdown ? "dropdown-menu active" : "dropdown-menu"}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="min">
+                <input
+                  type="number"
+                  placeholder="от"
+                  onKeyDown={(e) => {
+                    if (e.code === "Enter") {
+                      setDropdown(!dropdown);
+                    }
+                  }}
+                />
+              </div>
+              <div className="max">
+                <input
+                  type="number"
+                  placeholder="до"
+                  onKeyDown={(e) => {
+                    if (e.code === "Enter") {
+                      setDropdown(!dropdown);
+                    }
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="search-button">
+            <img src={search} alt="" />
+            <p>Искать</p>
+          </div>
+        </div>
       </div>
     </section>
   );
