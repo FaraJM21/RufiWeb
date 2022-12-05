623fe5988a8358d8e6e1cc424bac5b00e3ef8272
FaraJM21 <farruxrihsiyev@gmail.com>
2022-12-05 10:12:18 +0500

    footer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 export { default as Apartments } from "./apartments/Apartments";
-export { default as AbaoutApartment } from "./aboutApartment/AboutApartment";
+export { default as AbaoutApartment } from "./aboutapartment/AboutApartment";
 export { default as Profile } from "./profile/Profile";
 export { default as Layout } from "./layout/Layout";
 export {default as Home  } from "./home/Home"; 
