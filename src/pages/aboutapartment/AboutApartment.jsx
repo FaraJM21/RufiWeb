9c9504d16287472c5e784dcd04a724c7227ea4e2
FaraJM21 <farruxrihsiyev@gmail.com>
2022-12-05 11:07:19 +0500

    aboutaprtment started

--- src/pages/aboutapartment/AboutApartment.jsx
+++ src/pages/aboutapartment/AboutApartment.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function AboutApartment() {
   return (
-    <div>AboutApartment</div>
+    <div className='main'>
+      <div className="container">
+        <div className='aboutapartment'></div>
+      </div>
+    </div>
   )
 }
 
